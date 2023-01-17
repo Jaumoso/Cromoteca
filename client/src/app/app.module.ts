7f03b8df289bef1bbc792a5590eb1d303db81373
Jaume Civera <jaumecvr2000@gmail.com>
2023-01-17 20:01:15 +0100

    frontend iniciado

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+
+import { baseURL } from './shared/baseurl';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [
+    { provide: 'BaseURL', useValue: baseURL },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
