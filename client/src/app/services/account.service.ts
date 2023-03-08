6b21b62d17036e2b302eb526071bcd28cffafeb7
Jaume Civera <jaumecvr2000@gmail.com>
2023-03-08 10:52:05 +0100

    await incluido en create address y create user

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -22,11 +22,11 @@ export class AccountService {
 
   async createAccount(user: User, address: Address) {
     try {
-      this.addressService.createAddress(address)
+      await this.addressService.createAddress(address)
       .then((address) => {
         console.log("Dirección creada correctamente");
         user.addressId = address._id;
-        this.userService.createUser(user)
+        await this.userService.createUser(user)
         .then((user) => {
           console.log("Usuario creado correctamente");
           let intermediate = new Intermediate;
