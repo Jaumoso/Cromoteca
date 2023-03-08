82044240db6e0bf44c11ea50eae7df70495b9ee7
Jaume Civera <jaumecvr2000@gmail.com>
2023-03-08 13:59:18 +0100

    añadido el dto stub de address

---
+++ server/src/address/dto/address.dto.stub.ts
@@ -0,0 +1,9 @@
+export const AddressDtoStub = () => {
+    return {
+        street: 'default street',
+        city: 'default city',
+        postalCode: 'default postal code',
+        province: 'default province',
+        country: 'default country'
+    };
+};
